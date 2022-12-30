27a930e63298b8dd3a1650b1e8a82cb29a4322c0
Jason Anca <jason_anca@hotmail.com>
2022-12-30 21:41:23 +0000

    Added boxes and button for login page

--- movies-app/src/components/templateLoginPage/index.js
+++ movies-app/src/components/templateLoginPage/index.js
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import { Navigate, Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import { AuthContext } from "../../contexts/authContext"
 
 
-const TemplateLoginPage = ({ movie, children }) => {
+const TemplateLoginPage = props => {
+  const context = useContext(AuthContext)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-
+  const login = () => {
+    context.authenticate(username, password);
+  };
 
   return (
     <>
-        <h2>Login</h2>
-
-
+        <Box component ="div" pt={10} sx={{ display: 'flex'}}>
+          <Typography component="h2" variant="h3">
+            Log into TMDB
+          </Typography>
+        </Box>
+        <Box component ="div" pt={5} sx={{ display: 'flex'}}>
+          <TextField
+           id="username"
+            label="Username"
+           type="text"
+           autoFocus
+           onChange={e => {setUsername(e.target.value)}}
+          />
+        </Box>
+        <Box component ="div" pt={1} sx={{ display: 'flex'}}>
+          <TextField
+            id="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            onChange={e => {setPassword(e.target.value)}}
+          />
+        </Box>
+        <Box component ="div" pt={1} sx={{ display: 'flex'}}>
+          <Button
+          variant="contained"
+          color="primary"
+          onClick={login}
+          >
+            Log in
+          </Button>
+        </Box> 
     </>
   );
 };
 
-export default TemplateLoginPage; 
+export default TemplateLoginPage;
